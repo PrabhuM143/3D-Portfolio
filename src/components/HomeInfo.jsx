21541586731a5d73bd1d883ee0d4940c2344ee04
PrabhuM143 <prabhumurthy143@gmail.com>
2024-02-29 21:07:57 +0530

    added home info

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,25 +1,48 @@
 import React from 'react'
-
-const renderContent = {
-    1: (
-        <h1>1</h1>
-    ),
-    2: (
-        <h1>2</h1>
-    ),
-    3: (
-        <h1>3</h1>
-    ),
-    4: (
-        <h1>4</h1>
-    ),
-}
+import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
 
 const InfoBox = ({ text, link, btnText })=>(
     <div className='info-box'>
-
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={Link} className='neo-brutalism-white neo-btn'>
+            {btnText}
+            <img src={arrow} className='w-4 h-4 object-contain'/>
+        </Link>
     </div>
 )
+
+const renderContent = {
+    1: (
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+            Hi, I'm <span className='font-semibold'>Prabhu</span> 👋
+            <br />
+            FrontEnd Web Developer
+            </h1>
+    ),
+    2: (
+        < InfoBox 
+            text="Worked with many companies and learnt many skills along the way."
+            link="/about"
+            btnText="Learn more"
+        />
+    ),
+    3: (
+        < InfoBox 
+            text="Led multiple projects to success over the years. Curious about the impacts?"
+            link="/projects"
+            btnText="Visit my portfolio"
+        />
+    ),
+    4: (
+        < InfoBox 
+            text="Need a project done or looking for a dev? I'm just a few keystrokes away"
+            link="/contact"
+            btnText="Let's talk"
+        />
+    ),
+}
+
 
 const HomeInfo = ({ currentStage }) => {
   return renderContent [ currentStage ] || null;
