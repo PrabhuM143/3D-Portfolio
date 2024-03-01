492ff79e710ca9fa975b13b76752d85299363ed1
PrabhuM143 <prabhumurthy143@gmail.com>
2024-03-01 21:34:45 +0530

    added contact page

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { arrow } from '../assets/icons'
 
 const InfoBox = ({ text, link, btnText })=>(
     <div className='info-box'>
         <p className='font-medium sm:text-xl text-center'>{text}</p>
-        <Link to={Link} className='neo-brutalism-white neo-btn'>
+        <Link to={link} className='neo-brutalism-white neo-btn'>
             {btnText}
             <img src={arrow} className='w-4 h-4 object-contain'/>
         </Link>
